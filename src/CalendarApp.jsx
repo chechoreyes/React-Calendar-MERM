8e5967b70c13eddcdab37d6484535a3d731642f9
Sergio Reyes Gajardo <sergio.reyes.gajardo@gmail.com>
2022-07-29 18:03:28 -0400

    project intialized

---
+++ src/CalendarApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRouter } from './Router';
+
+export const CalendarApp = () => {
+    return (
+        <BrowserRouter>
+            <AppRouter />
+        </BrowserRouter>
+    );
+};
